596a7af4116c50dad2ec6983ebaadc8bd1cd1a3a
ArnaudChaigne <93703424+ArnaudChaigne@users.noreply.github.com>
2023-06-02 15:16:31 +0200

    Ajout barre de recherche + data en memoire

--- lib/data/dataSource/remote/VlilleApi.dart
+++ lib/data/dataSource/remote/VlilleApi.dart
@@ -7,10 +7,16 @@ import 'package:http/http.dart' as http;
 
 class VlilleApi {
 
+  VlilleApiResponse? memory;
+
   Future<VlilleApiResponse> getVlille() async {
-    Uri url = Uri.parse("https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=vlille-realtime&q=&facet=nom&facet=commune&facet=etat&facet=type&facet=etatconnexion&rows=300");
-    http.Response response = await http.get(url);
-    return compute(vlilleApiResponseFromJson, response.body);
+    if (memory == null) {
+      Uri url = Uri.parse(
+          "https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=vlille-realtime&q=&facet=nom&facet=commune&facet=etat&facet=type&facet=etatconnexion&rows=300");
+      http.Response response = await http.get(url);
+      memory = await compute(vlilleApiResponseFromJson, response.body);
+    }
+    return Future.value(memory);
   }
 
   VlilleApiResponse vlilleApiResponseFromJson(String body) {
